62ea8f9959687c84777bd1dd84443ac3110adea2
Zachary Folk <zacharyfolk@gmail.com>
2020-09-09 23:12:57 -0700

    Remove default github stuff, start playing with weather api

---
+++ index.js
@@ -0,0 +1,33 @@
+const request = require('request');
+const key = require('./keys');
+let apiKey = key.getWeatherKey();
+let city = 'seattle';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+function convertUnix(stamp) {
+  var date = new Date(stamp * 1000);
+  var hours = date.getHours();
+  var minutes = "0" + date.getMinutes();
+  var seconds = "0" + date.getSeconds();
+  var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+  return formattedTime;
+}
+
+request(url, function(err, response, body) {
+  if (err) {
+    console.log('error:', error);
+  } else {
+    // console.log('body:', body);
+    let weather = JSON.parse(body);
+    let sunrise = `${weather.sys.sunrise}`
+    let sunset = `${weather.sys.sunset}`
+    let message = `It's ${weather.main.temp} degrees in ${weather.name}!`
+    console.log(message);
+    // console.log(JSON.parse(body));
+    console.log( 'Sunrise : ' + convertUnix(sunrise) + '\nSunset : ' + convertUnix(sunset))
+// get timezone for location to compare to current time
+// What a list!! https://stackoverflow.com/questions/16086962/how-to-get-a-time-zone-from-a-location-using-latitude-and-longitude-coordinates/16086964
+
+
+  }
+});
